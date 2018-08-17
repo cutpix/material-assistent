eb278afe9bfd662f81ca49890cf9a552993a2cf0
Andrei Pavlyuk <devdice@outlook.com>
2018-08-17 07:58:56 +0300

    Added material module with components and render them.

--- README.md
+++ README.md
@@ -25,3 +25,9 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+## Getting Started with Angular 6 Material 
+
+## YouTube
+
+[Getting Started with Angular 6 Material (Tutorial)](https://www.youtube.com/watch?v=Fcr-gM-QThc)
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatMenuModule
+} from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
     MatButtonModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule
   ],
   exports: [
     MatButtonModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule
   ],
   declarations: []
 })
